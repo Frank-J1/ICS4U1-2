855f0b6107f6274460524f6ca6ec059365f771ac
Frank Jardim <Frankmartinscnd@gmail.com>
2024-03-28 11:36:19 -0400

    Initial commit

---
+++ popUp-java/src/mainFrame.java
@@ -0,0 +1,16 @@
+import java.util.HexFormat;
+
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+// import java class packages - 
+
+public class mainFrame extends JFrame {
+    public void init(){
+        setTitle("Pop Up Window!! :):)");
+        setSize(600, 300);
+        setDefaultCloseOperation
+        (WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
